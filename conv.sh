9566498abc460eac600ba6272d2015c520d44c4e
Justine Leon Uro <justineuro@gmail.com>
2017-09-24 08:06:53 +0800

    2017-09-24 updates

--- conv.sh
+++ conv.sh
@@ -3,13 +3,13 @@ inkscape -D -z --file=MP002.svg --export-pdf=MP002.pdf --export-latex
 
 # history | tail -n 10
 sed -i.OLD 's/\\smash{Marche/\\smash{\\bf\\Large Marche/g' MP*.pdf_tex 
-sed -i.OLD 's/\\smash{Allegretto/\\smash{\\bf\\small\\hspace{-0.10in}Allegretto/g' MP*.pdf_tex 
-sed -i.OLD 's/\\smash{f}/\\smash{\\em \\large f}/g' MP*.pdf_tex 
-sed -i.OLD 's/\\smash{p}/\\smash{\\em \\large p}/g' MP*.pdf_tex 
-sed -i.OLD 's/\\smash{ff}/\\smash{\\em \\large ff}/g' MP*.pdf_tex
-sed -i.OLD 's/\\smash{cresc.}/\\smash{\\em \\large cresc.}/g' MP*.pdf_tex 
-sed -i.OLD 's/\\smash{dim.}/\\smash{\\em \\large dim.}/g' MP*.pdf_tex 
-sed -i.OLD 's/\\smash{cre -}/\\smash{\\em \\large cre -}/g' MP*.pdf_tex 
-sed -i.OLD 's/\\smash{scen -}/\\smash{\\em \\large scen -}/g' MP*.pdf_tex 
-sed -i.OLD 's/\\smash{-}/\\smash{\\em \\large -}/g' MP*.pdf_tex
-sed -i.OLD 's/\\smash{- do}/\\smash{\\em \\large - do}/g' MP*.pdf_tex 
+sed -i.OLD 's/\\smash{Allegretto/\\smash{\\bf\\small\\hspace{-0.10in}Allegretto/g' MP*.pdf_tex
+sed -i.OLD 's/\\smash{f}/\\smash{\\textbf{$\\maybebm{f}$}}/g' MP*.pdf_tex 
+sed -i.OLD 's/\\smash{p}/\\smash{\\textbf{$\\maybebm{p}$}}/g' MP*.pdf_tex 
+sed -i.OLD 's/\\smash{ff}/\\smash{\\textbf{$\\maybebm{f}\\hspace{-0.05in}\\maybebm{f}$}}/g' MP*.pdf_tex
+sed -i.OLD 's/\\smash{cresc.}/\\smash{\\textbf{$\\maybebm{cresc.}$}}/g' MP*.pdf_tex 
+sed -i.OLD 's/\\smash{dim.}/\\smash{\\textbf{$\\maybebm{dim.}$}}/g' MP*.pdf_tex 
+sed -i.OLD 's/\\smash{cre -}/\\smash{\\textbf{$\\maybebm{cre}$ -}}/g' MP*.pdf_tex 
+sed -i.OLD 's/\\smash{scen -}/\\smash{\\textbf{$\\maybebm{scen}$ -}}/g' MP*.pdf_tex 
+sed -i.OLD 's/\\smash{-}/\\smash{\\textbf{-}}/g' MP*.pdf_tex
+sed -i.OLD 's/\\smash{- do}/\\smash{\\textbf{\\hspace{-0.10in}- $\\maybebm{do}$}}/g' MP*.pdf_tex 
